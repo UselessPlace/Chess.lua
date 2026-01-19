a33cc1017d00b0bef5b01d1ae19c16c12cf050f8
UselessPlace <203527339+UselessPlace@users.noreply.github.com>
2026-01-19 10:27:32 +0000

    Fixing

--- board.lua
+++ board.lua
@@ -1,9 +1,9 @@
-local Row1 = {" R", " N", " B", "K"," Q", " B", " N", "R"}
-local Row2 = {" P"," P"," P"," P"," P"," P"," P"," P"}
+local Row1 = {" R,", " N,", " B,", "K,"," Q,", " B,", " N,", "R,"}
+local Row2 = {" P,"," P,"," P,"," P,"," P,"," P,"," P,"," P,"}
 
 
 
 function DrawBoard()
-    print(Row1)
-    print(Row2)
+    print(table.concat(Row1, " "))
+print(table.concat(Row2, " "))
 end
