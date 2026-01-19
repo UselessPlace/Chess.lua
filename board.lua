15ceddfd94d03ca2d6cffd145c6a51c59ce7ca12
UselessPlace <aamoskettunen12@gmail.com>
2026-01-19 19:46:01 +0200

    added the ability to select pieces. but not move them yet :/

--- board.lua
+++ board.lua
@@ -1,9 +1,63 @@
-local Row1 = {" R,", " N,", " B,", "K,"," Q,", " B,", " N,", "R,"}
-local Row2 = {" P,"," P,"," P,"," P,"," P,"," P,"," P,"," P,"}
+local Row1 = {"R", "N", "B", "K","Q", "B", "N", "R"}
+local Row2 = {"P","P","P","P","P","P","P","P"}
+local Row3 = {"X","X","X","X","X","X","X","X",}
+
+----save data? meaby i guess so. like kinda
+
+local WholeBoard = { {"R", "N", "B", "K","Q", "B", "N", "R"} ,
+                    {"P","P","P","P","P","P","P","P"} }
+
+
+-----
+
+
+
+local numbers = {"3","4","5","6","7","8"}
+function DrawBoardPieces()
+print(table.concat(Row1, " ").. "  1")
+print(table.concat(Row2, " ").. "  2")
+
+    for i=1,4  do
+        print(table.concat(Row3, " ").."  "..numbers[i])
+        i = i + 1
+        
+    end
+    
+print(table.concat(Row2, " ").. "  7")
+print(table.concat(Row1, " ").. "  8")
+
+end
 
 
 
 function DrawBoard()
-    print(table.concat(Row1, " "))
-print(table.concat(Row2, " "))
+    
+
+DrawBoardPieces()
+print("")
+    print("A B C D E F G H")
+
+SaveBoardPieces()
+end
+
+
+function SaveBoardPieces()
+
+
+
+end
+
+function GetPiece(PieceX,PieceY)
+
+    print("Piece selected: "WholeBoard[PieceX][PieceY])
+    
+
+
+
+end
+function MovePiece()
+
+print("lol")
+
+
 end
--- main.lua
+++ main.lua
@@ -1,3 +1,55 @@
 require("board")
 
-DrawBoard()
+local Play = true
+
+
+function SelectPiece()
+
+print("select piece by using coordinates:")
+print("Input text axis (example e)")
+PieceX = io.read("*number")
+if PieceX then
+print("Input number axis (example 4)")
+PieceY = io.read("*number")
+     if PieceY then
+
+     GetPiece(PieceX,PieceY)
+
+     end
+
+else
+
+print("bruh. stupid")
+Play = true
+
+
+
+end
+
+end
+
+
+
+
+
+
+
+
+
+while true do
+if Play == true then
+Play = false 
+
+
+DrawBoard() --Draws board :D
+
+SelectPiece()
+MovePiece()
+
+end
+
+
+
+end
+
+
